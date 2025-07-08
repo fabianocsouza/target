67db29f8ec903e9fa23c990f566af3c0c7a3d5b5
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-08 15:23:03 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "@/theme/colors";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Oi, React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
