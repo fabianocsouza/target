105722ecae977e2106998305f559ae5c902805ec
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-09 20:07:32 -0300

    add `expo-font @expo-google-fonts/inter`

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
+import { fontFamily } from "@/theme/fontfamily";
 import { router } from "expo-router";
 import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Expo Router</Text>
+      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>
+        Expo Router
+      </Text>
       <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
       <Button
         title="Transação"
