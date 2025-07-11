abc791ce5a15d679431a63ac4c01953d52077f62
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-11 15:07:17 -0300

    create components`HomeHeader` and `Separator`

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,42 @@
+import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { colors } from "@/theme/colors";
+import { Separator } from "../Separator";
+import { Summary, type SummaryProps } from "../Summary";
+export type HomeHeaderProps = {
+  total: string;
+  input: SummaryProps;
+  output: SummaryProps;
+};
+
+type Props = {
+  data: HomeHeaderProps;
+};
+
+export function HomeHeader({ data }: Props) {
+  return (
+    <LinearGradient
+      colors={[colors.blue[500], colors.blue[800]]}
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
+      </View>
+      <Separator color={colors.blue[400]} />
+      <View style={styles.summary}>
+        <Summary
+          data={data.input}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+        <Summary
+          data={data.output}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+          isLeft={true}
+        />
+      </View>
+    </LinearGradient>
+  );
+}
