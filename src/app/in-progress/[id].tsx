78f40898fb5c5398e2c86dd2f186a66578a081be
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-12 12:41:39 -0300

    create components `Progress`

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,12 +1,26 @@
-import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+import { Button } from "@/components/Button";
+import { Progress } from "@/components/Progress";
+import { PageHeader } from "@/components/PageHeader";
+
+const details = {
+  current: "580,00",
+  target: "1.790,00",
+  percentage: 25,
+};
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>ID: {params.id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{ icon: "edit", onPress: () => {} }}
+      />
+      <Progress data={details} />
+      <Button title="Nova transação" />
     </View>
   );
 }
