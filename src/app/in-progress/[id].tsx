7cdd57f830ef1033a062cd2e141f767161fffacd
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-12 13:26:15 -0300

    create components `Transaction`

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,15 +1,42 @@
 import { View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 
 import { Button } from "@/components/Button";
 import { Progress } from "@/components/Progress";
 import { PageHeader } from "@/components/PageHeader";
+import { Transaction } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { List } from "@/components/List";
 
 const details = {
   current: "580,00",
   target: "1.790,00",
   percentage: 25,
 };
+
+const transactions = [
+  {
+    id: "1",
+    value: "20,00",
+    date: "12/04/25",
+    description: "",
+    type: TransactionTypes.Output,
+  },
+  {
+    id: "2",
+    value: "300,00",
+    date: "12/04/25",
+    description: "CDB de 110% no banco XPTO",
+    type: TransactionTypes.Input,
+  },
+  {
+    id: "3",
+    value: "300,00",
+    date: "12/04/25",
+    description: "CDB de 110% no banco XPTO",
+    type: TransactionTypes.Input,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -20,7 +47,21 @@ export default function InProgress() {
         rightButton={{ icon: "edit", onPress: () => {} }}
       />
       <Progress data={details} />
-      <Button title="Nova transação" />
+
+      <List
+        title="Transações"
+        data={transactions}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+        emptyMessage="Nenhum transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+      />
+
+      <Button
+        title="Nova transação"
+        onPress={() => router.navigate(`/transaction/${params.id}`)}
+      />
     </View>
   );
 }
---
+++ src/components/Transaction/index.tsx
@@ -0,0 +1,49 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { colors } from "@/theme";
+
+type TransactionProps = {
+  id: string;
+  value: string;
+  date: string;
+  description?: string;
+  type: TransactionTypes;
+};
+
+type Props = {
+  data: TransactionProps;
+  onRemove: () => void;
+};
+
+export function Transaction({ data, onRemove }: Props) {
+  return (
+    <View style={styles.container}>
+      <MaterialIcons
+        name={
+          data.type === TransactionTypes.Input
+            ? "arrow-upward"
+            : "arrow-downward"
+        }
+        size={20}
+        color={
+          data.type === TransactionTypes.Input
+            ? colors.blue[500]
+            : colors.red[400]
+        }
+      />
+      <View style={styles.info}>
+        <Text style={styles.value}>R$ {data.value}</Text>
+        <Text style={styles.description} numberOfLines={1}>
+          {data.date} {data.description && `• ${data.description}`}
+        </Text>
+      </View>
+
+      <TouchableOpacity>
+        <MaterialIcons name="close" size={18} color={colors.gray[500]} />
+      </TouchableOpacity>
+    </View>
+  );
+}
