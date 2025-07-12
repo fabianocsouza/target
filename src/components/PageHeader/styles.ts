bbda299f7b6a67dd74dd3fed80621d0e7a880fe3
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-12 11:28:46 -0300

    create components `PageHeader`

---
+++ src/components/PageHeader/styles.ts
@@ -0,0 +1,23 @@
+import { colors, fontFamily } from "@/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: { width: "100%", paddingTop: 32 },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: fontFamily.bold,
+    color: colors.black,
+    marginBottom: 7,
+  },
+  subTitle: {
+    fontSize: 14,
+    fontFamily: fontFamily.regular,
+    color: colors.gray[500],
+  },
+});
