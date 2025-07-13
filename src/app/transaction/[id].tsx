3574e1a89f0c9aa703d64c174977289506692e29
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-07-13 06:55:59 -0300

    create components `TransactionType` and screen `Transaction`

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,12 +1,33 @@
-import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 
-export default function transaction() {
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { PageHeader } from "@/components/PageHeader";
+import { CurrencyInput } from "@/components/CurrencyInput";
+import { TransactionType } from "@/components/TransactionType";
+import { useState } from "react";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+
+export default function Transaction() {
+  const [type, setType] = useState(TransactionTypes.Input);
   const params = useLocalSearchParams<{ id: string }>();
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>ID: {params.id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Nova transação"
+        subTitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
+      />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
+        <CurrencyInput label="Valor (R$)" value={0} />
+        <Input
+          label="Motivo (opcional)"
+          placeholder="Ex: Investir em CDB de 110% no banco XPTO"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
